c35cd4c2d5addf024c5802d59be55dcdc9c260a2
YY <550872569@qq.com>
2018-10-09 14:50:50 +0800

    add rc demo

---
+++ ios-rongimdemo/RCDCSAnnounceView.h
@@ -0,0 +1,23 @@
+//
+//  RCDCSAnnounceView.h
+//  SealTalk
+//
+//  Created by Yan on 2018/10/9.
+//  Copyright © 2018 RongCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+
+@protocol RCDCSAnnounceViewDelegate <NSObject>
+
+@end
+
+@interface RCDCSAnnounceView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-rongimdemo/RCDCSAnnounceView.m
@@ -0,0 +1,21 @@
+//
+//  RCDCSAnnounceView.m
+//  SealTalk
+//
+//  Created by Yan on 2018/10/9.
+//  Copyright © 2018 RongCloud. All rights reserved.
+//
+
+#import "RCDCSAnnounceView.h"
+
+@implementation RCDCSAnnounceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
